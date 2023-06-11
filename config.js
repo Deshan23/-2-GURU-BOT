df61d8569f51c89374c7389c199a812b5b33a594
Deshan23 <135618982+Deshan23@users.noreply.github.com>
2023-06-11 12:36:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94771360470', 'sandaru', true],
+  ['94771360470', 'Deahan', true], 
   [''] 
 ] //Number of owners
 
@@ -47,17 +47,17 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'DESHAN'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
-global.dbase = 'mongodb://mongo:q4bHb1TK9mOyuKFmp1oY@containers-us-west-56.railway.app:6244' //ADD YOUR MONGODB BY CHANGIMG IT
+global.packname = 'DESHAN┃ᴮᴼᵀ' 
+global.author = '@madu' 
+global.igfg = '' 
+global.dygp = ''
+global.fgsc = 
+global.fgyt = 
+global.fgpyp = 
+global.fglog 
+global.dbase = 
 
 global.wait = '*LOADING PLEASE WAIT....*'
 global.rwait = '⌛'
